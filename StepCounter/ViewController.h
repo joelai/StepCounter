4640aea90e70dc4047b13232234bf660eda779ab
Joe Lai <joe.lai.cn@gmail.com>
2015-06-11 17:29:18 +0800

    Initial Commit

---
+++ StepCounter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StepCounter
+//
+//  Created by Joe on 15/6/11.
+//  Copyright (c) 2015年 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
